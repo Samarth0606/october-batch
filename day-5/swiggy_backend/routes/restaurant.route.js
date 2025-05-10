91fa0dced03572b784e0e1307ffc9625915b7edf
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-05-10 21:09:56 +0530

    day 5 codes

--- day-5/swiggy_backend/routes/restaurant.route.js
+++ day-5/swiggy_backend/routes/restaurant.route.js
@@ -1,6 +1,10 @@
-import { createRestaurant } from "../controllers/restaurant.controller.js";
+import { createRestaurant, deleteRestaurant, fetchRestaurant, updateRestaurant } from "../controllers/restaurant.controller.js";
 
 
 export function restRoutes(app){
-    app.post('/api/restaurant' , createRestaurant)
+    app.post('/api/restaurant' ,createRestaurant);
+    app.get('/api/restaurants' , fetchRestaurant);
+    app.put('/api/restaurant/:id' , updateRestaurant);
+    app.delete('/api/restaurant/:id' , deleteRestaurant);
+
 }
