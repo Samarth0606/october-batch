5b181aa8b6f57fcc44a645534bf6684089c83888
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-05-10 19:58:14 +0530

    day 5 node october batch middle

---
+++ day-5/swiggy_backend/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app = express();
+import mongoose from 'mongoose';
+
+// DB connect
+mongoose.connect('mongodb+srv://samarthvohraindia:vIPWvMklqMZTmptI@cluster0.9cbny99.mongodb.net/')
+.then(()=>{
+    console.log("DB CONNECTED SUCCESSFULLY");
+})
+.catch((err)=>{
+    console.log(err , "err while connecting DB");
+})
+
+app.use(express.json()) //body parsing middleware
+
+app.use((req,res,next)=>{
+    console.log("middleware hu mai");
+    next();
+})
+
+app.get('/' , (req,res)=>{
+    res.send("welcome to root route")
+})
+
+const PORT = 8000;
+app.listen(PORT , ()=>{
+    console.log(`SERVER CONNECTED AT PORT: ${PORT}`);
+})
+
+
+// samarthvohraindia
+// vIPWvMklqMZTmptI
+// mongodb+srv://samarthvohraindia:vIPWvMklqMZTmptI@cluster0.9cbny99.mongodb.net/
+
+
+
+
+
+
+
+
+
+
